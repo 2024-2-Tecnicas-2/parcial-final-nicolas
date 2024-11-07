f49e737069baa7895530ddd3fc44351d2887e381
dfasd <DSAFASD@SADFASD.COM>
2024-11-06 19:17:19 -0500

    CODIGO

--- src/main/java/logicaNegocio/Animal.java
+++ src/main/java/logicaNegocio/Animal.java
@@ -3,3 +3,113 @@ package logicaNegocio;
 public abstract class Animal {
     // TODO: Aquí va tu código
 }
+// Animal.java
+public abstract class Animal {
+    protected TipoHabitat habitat;
+    
+    public abstract String emitirSonido();
+    public abstract String obtenerDieta();
+    
+    public TipoHabitat getHabitat() {
+        return habitat;
+    }
+}
+
+// TipoHabitat.java
+public enum TipoHabitat {
+    ACUATICO,
+    TERRESTRE,
+    AEREO
+}
+
+// Leon.java
+public class Leon extends Animal {
+    public Leon() {
+        this.habitat = TipoHabitat.TERRESTRE;
+    }
+    
+    @Override
+    public String emitirSonido() {
+        return "Rugido";
+    }
+    
+    @Override
+    public String obtenerDieta() {
+        return "Carnívoro";
+    }
+}
+
+// Delfin.java
+public class Delfin extends Animal {
+    public Delfin() {
+        this.habitat = TipoHabitat.ACUATICO;
+    }
+    
+    @Override
+    public String emitirSonido() {
+        return "Chirrido";
+    }
+    
+    @Override
+    public String obtenerDieta() {
+        return "Pescado";
+    }
+}
+
+// Domestico.java
+public interface Domestico {
+    String interactuarConHumano();
+}
+
+// Perro.java
+public class Perro extends Animal implements Domestico {
+    public Perro() {
+        this.habitat = TipoHabitat.TERRESTRE;
+    }
+    
+    @Override
+    public String emitirSonido() {
+        return "Ladrido";
+    }
+    
+    @Override
+    public String obtenerDieta() {
+        return "Omnívoro";
+    }
+    
+    @Override
+    public String interactuarConHumano() {
+        return "El perro mueve la cola y ladra de felicidad";
+    }
+}
+
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        // Crear instancias de los animales
+        Leon leon = new Leon();
+        Delfin delfin = new Delfin();
+        Perro perro = new Perro();
+        
+        // Mostrar información del León
+        System.out.println("León:");
+        System.out.println("Sonido: " + leon.emitirSonido());
+        System.out.println("Dieta: " + leon.obtenerDieta());
+        System.out.println("Hábitat: " + leon.getHabitat());
+        System.out.println();
+        
+        // Mostrar información del Delfín
+        System.out.println("Delfín:");
+        System.out.println("Sonido: " + delfin.emitirSonido());
+        System.out.println("Dieta: " + delfin.obtenerDieta());
+        System.out.println("Hábitat: " + delfin.getHabitat());
+        System.out.println();
+        
+        // Mostrar información del Perro
+        System.out.println("Perro:");
+        System.out.println("Sonido: " + perro.emitirSonido());
+        System.out.println("Dieta: " + perro.obtenerDieta());
+        System.out.println("Hábitat: " + perro.getHabitat());
+        System.out.println("Interacción: " + perro.interactuarConHumano());
+    }
+}
